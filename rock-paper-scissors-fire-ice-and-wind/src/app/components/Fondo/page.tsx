193094c9ae67a30bb71512f870dd85386e360a5b
David Gamiz <calebfx12@gmail.com>
2023-08-09 13:32:28 -0600

    Cambios

---
+++ rock-paper-scissors-fire-ice-and-wind/src/app/components/Fondo/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./page.css";
+
+export const Background = (): JSX.Element => {
+  return (
+    <div className="image">
+      <img className="img" alt="Image" src="fondo.png" />
+    </div>
+  );
+};
